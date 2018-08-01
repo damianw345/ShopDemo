c81ce6e581947587e34ff524afd907240391c2d5
damianw345 <damianw345@gmail.com>
2018-08-01 23:24:20 +0200

    fixes
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/main/java/com/github/damianw345/shopdemo/controller/FlavourController.java
+++ src/main/java/com/github/damianw345/shopdemo/controller/FlavourController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-@RequestMapping("/dressing")
+@RequestMapping("/flavour")
 public class FlavourController {
 
     private final FlavourService flavourService;
