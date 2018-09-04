d99930c33a142909b47093c0b66dd49e65545e6c
damianw345 <damianw345@gmail.com>
2018-09-04 18:09:31 +0200

    Removing unused files
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./gradlew build
-ssh ubuntu@aws -v "kill $(pgrep java); rm shop-demo.jar;"
+ssh ubuntu@aws "kill $(pgrep java); rm shop-demo.jar;"
 scp ./build/libs/shop-demo-0.0.1-SNAPSHOT.jar ubuntu@aws:shop-demo.jar
 ssh ubuntu@aws "screen -m -d java -jar shop-demo.jar;"
-
